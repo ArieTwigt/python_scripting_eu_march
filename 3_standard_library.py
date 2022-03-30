9af247a68e644a890d760f0e7ab7f4b7b4253c73
arietwigt <twigt.arie@gmail.com>
2022-03-30 14:50:51 +0200

    added examples for module imports

---
+++ 3_standard_library.py
@@ -0,0 +1,27 @@
+#%% import the entire library
+import datetime
+
+current_moment = datetime.datetime.now()
+
+#%% only import specific parts from the library
+from datetime import datetime, timedelta
+
+current_moment = datetime.now()
+print(current_moment)
+later_moment   = current_moment + timedelta(hours=3)
+print(later_moment)
+
+# %% import a library as an alias
+import datetime as dt
+
+current_moment = dt.datetime().now()
+
+
+# %% import a specific part of a library as an alias
+from datetime import datetime as dt, timedelta as td
+
+current_moment = dt.now()
+print(current_moment)
+later_moment   = current_moment + td(hours=3)
+print(later_moment)
+# %%
